2548a9baa497eb2f2ebfdb7784308bb82a148975
Philippe chambon <joan_krugg@live.fr>
2017-09-13 16:27:29 +0200

    add artists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/show'
+
+  get 'artists/edit'
+
+  get 'artists/update'
+
+  get 'artists/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
