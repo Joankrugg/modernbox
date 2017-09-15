5864537ce8c7064004fa2a0d13548f29d43b0c55
Philippe chambon <joan_krugg@live.fr>
2017-09-15 23:14:29 +0200

    home page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
   root to: 'pages#home'
   resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
